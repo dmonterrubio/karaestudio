bf0aaf61a13d2f9b2be793e55af7d3df4b86535b
diegomonterrubio <diego.monterrubio@marfeel.com>
2021-10-07 12:32:18 +0200

    add instagram

--- src/pages/Contacto/index.jsx
+++ src/pages/Contacto/index.jsx
@@ -22,8 +22,8 @@ const Contacto = () => {
                     <p><a href="mailto:info@karaestudio.com">info@karaestudio.com</a> </p>
                     <p className="contacto_ara">Ara <a href="tel:+34660589422">+34 620 978 762</a></p>
                     <p className="contacto_andrea">Andrea <a href="tel:+34660589422">+34 616 353 949</a></p>
-                    <p><a href="instagram.com">Instagram</a></p>
-                    <p><a href="linkedin.com">Linkedin</a></p>
+                    <p><a href="https://www.instagram.com/kara.estudio/">Instagram</a></p>
+                    {/* <p><a href="linkedin.com">Linkedin</a></p> */}
                 </div>
             </div>
             <div className="media">
